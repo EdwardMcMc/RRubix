9d75abc461d13d7df23b5e9553feca3c47464a4b
EdwardMcMc <Edward.McKenzie-McHarg@hotmail.com>
2020-02-01 13:59:42 +1100

    add buttons and implode code simplicity

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './index.css';
 import Board from './components/Board';
 import ColorPicker from './components/ColorPicker'
+import Button from './components/Button';
 
 class App extends React.Component {
   constructor(props){
@@ -25,7 +26,61 @@ class App extends React.Component {
     }
   }
 
-  
+  changeColor = (x,y) => {
+    console.log('change color')
+    let newBoard=this.state.board
+    console.log('newboard')
+    newBoard[y][x]=this.state.selectedColor
+    this.setState({board:newBoard})
+    console.log('stateset');
+  }
+
+  setColor =(setColor) =>{
+    if(setColor!==undefined)
+    {
+      this.setState({selectedColor:setColor})
+    console.log('set:'+setColor+'selec:'+this.state.selectedColor)
+    }
+    
+  }
+
+  Rotate(board,x,y,clockwise)
+  {
+    let topLeft=board[y-1][x-1]
+    let top=board[y-1][x]
+    let topRight=board[y-1][x+1]
+    let right=board[y][x+1]
+    let bottomRight=board[y+1][x+1]
+    let bottom=board[y+1][x]
+    let bottomLeft=board[y+1][x-1]
+    let left=board[x-1][y]
+
+    if(clockwise){
+      board[y-1][x-1]=bottomLeft
+      board[y+1][x-1]=bottomRight
+      board[y+1][x+1]=topRight
+      board[y-1][x+1]=topLeft
+
+      board[y-1][x]=left
+      board[x-1][y]=bottom
+      board[y+1][x]=right
+      board[y][x+1]=top
+
+    }
+    else{
+      board[y-1][x-1]=bottomRight
+      board[y+1][x-1]=bottomLeft
+      board[y+1][x+1]=topLeft
+      board[y-1][x+1]=topRight
+
+      board[y-1][x]=right
+      board[x-1][y]=top
+      board[y+1][x]=left
+      board[y][x+1]=bottom
+    }
+
+    return board
+  }
 
   F=(Board)=>{
     let oldBoard=this.state.board
@@ -56,14 +111,15 @@ class App extends React.Component {
     newBoard[4][6]=oldBoard[2][4]
     newBoard[3][6]=oldBoard[2][3]
 
-    newBoard[3][3]=oldBoard[5][3]
-    newBoard[4][3]=oldBoard[5][4]
-    newBoard[5][3]=oldBoard[5][5]
-    newBoard[5][4]=oldBoard[4][5]
-    newBoard[5][5]=oldBoard[3][5]
-    newBoard[4][5]=oldBoard[3][4]
-    newBoard[3][5]=oldBoard[3][3]
-    newBoard[3][4]=oldBoard[4][3]
+    // newBoard[3][3]=oldBoard[5][3]
+    // newBoard[4][3]=oldBoard[5][4]
+    // newBoard[5][3]=oldBoard[5][5]
+    // newBoard[5][4]=oldBoard[4][5]
+    // newBoard[5][5]=oldBoard[3][5]
+    // newBoard[4][5]=oldBoard[3][4]
+    // newBoard[3][5]=oldBoard[3][3]
+    // newBoard[3][4]=oldBoard[4][3]
+    this.Rotate(newBoard,4,4,true)
 
 
     this.setState({board:newBoard})
@@ -75,37 +131,28 @@ class App extends React.Component {
     this.F();
   } 
 
-  changeColor = (x,y) => {
-    console.log('change color')
-    let newBoard=this.state.board
-    console.log('newboard')
-    newBoard[y][x]=this.state.selectedColor
-    this.setState({board:newBoard})
-    console.log('stateset');
-  }
-
-  setColor =(setColor) =>{
-    if(setColor!==undefined)
-    {
-      this.setState({selectedColor:setColor})
-    console.log('set:'+setColor+'selec:'+this.state.selectedColor)
-    }
-    
-  }
-
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <h1>RRubix</h1>
-          
+          RRubix
         </header>
-        <body clgitssName='App-body'>
-        <Board board={this.state.board}changeColor={this.changeColor}/>
+        <body className='App-body'>
+           
+          <table>
+            <tbody>
+              <tr className='boardAndButtons'>
+                <td>
+                  <Button function={this.F} text='F'/>
+                  <Button function={this.f.bind(this)} text='f'/>
+                </td>
+                <td>
+                <Board board={this.state.board}changeColor={this.changeColor}/>  
+                </td>
+              </tr>
+            </tbody>
+          </table>
           <ColorPicker selectedColor={this.state.selectedColor} setColor={this.setColor}/>
-          {/* <button onClick={this.F()}>F</button> */}
-          <button onClick={this.F}>F</button>
-          <button onClick={this.f.bind(this)}>f</button>
         </body>
       </div>
     );
