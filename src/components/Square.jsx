f6397597753113fe3d02034cdaf1680f7c1cb1cf
EdwardMcMc <Edward.McKenzie-McHarg@hotmail.com>
2020-02-09 16:41:30 +1100

    comment out development aid

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -8,7 +8,7 @@ class Square extends React.Component{
   render(){
   return (
   <motion.div initial={{scale:0.8}} whileHover={{scale:1}} whileTap={{scale:0.8}} className={this.props.color} onClick={this.props.changeColor.bind(this,this.props.x,this.props.y)} >
-    [{this.props.y},{this.props.x}]
+    {/* [{this.props.y},{this.props.x}] */}
     </motion.div>
   );
   }
